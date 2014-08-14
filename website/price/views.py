6f791b0cd5d08d0ea4930acf74d70044a1943a89
denil1111 <denil1111@126.com>
2014-08-14 08:50:48 +0800

    img

--- website/price/views.py
+++ website/price/views.py
@@ -26,7 +26,7 @@ def index(req):
 		today=datetime.date.today()
 		today=today.strftime("%Y-%m-%d")
 		print (today)
-		res=yunxing(req.POST['departure'],req.POST['arrival'],req.POST['date'],today)
+		res=yunxing(req.POST['departure'],req.POST['arrival'],req.POST['date'],today,'price/templates/price/css/trend.png')
 		c = {'dep':findair(req.POST['departure']),
 			 'arr':findair(req.POST['arrival']),
 			 'date':req.POST['date'],
